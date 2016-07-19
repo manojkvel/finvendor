cfe037db183dcfd902f925ba4c7f2ae6171fd55b
Gaurav <3mcsltd@gmail.com>
2016-07-19 20:25:54 +0530

    Vendor search

--- src/com/finvendor/model/VendorSearchResult.java
+++ src/com/finvendor/model/VendorSearchResult.java
@@ -38,4 +38,53 @@ public class VendorSearchResult {
 	public void setVendorType(String vendorType) {
 		this.vendorType = vendorType;
 	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((vendorCompany == null) ? 0 : vendorCompany.hashCode());
+		result = prime * result + ((vendorCountry == null) ? 0 : vendorCountry.hashCode());
+		result = prime * result + ((vendorId == null) ? 0 : vendorId.hashCode());
+		result = prime * result + ((vendorName == null) ? 0 : vendorName.hashCode());
+		result = prime * result + ((vendorType == null) ? 0 : vendorType.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		VendorSearchResult other = (VendorSearchResult) obj;
+		if (vendorCompany == null) {
+			if (other.vendorCompany != null)
+				return false;
+		} else if (!vendorCompany.equals(other.vendorCompany))
+			return false;
+		if (vendorCountry == null) {
+			if (other.vendorCountry != null)
+				return false;
+		} else if (!vendorCountry.equals(other.vendorCountry))
+			return false;
+		if (vendorId == null) {
+			if (other.vendorId != null)
+				return false;
+		} else if (!vendorId.equals(other.vendorId))
+			return false;
+		if (vendorName == null) {
+			if (other.vendorName != null)
+				return false;
+		} else if (!vendorName.equals(other.vendorName))
+			return false;
+		if (vendorType == null) {
+			if (other.vendorType != null)
+				return false;
+		} else if (!vendorType.equals(other.vendorType))
+			return false;
+		return true;
+	}
 }
