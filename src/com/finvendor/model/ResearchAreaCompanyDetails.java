86724de628374f314c75dcd6c4f484a580c4a78b
Gaurav <3mcsltd@gmail.com>
2017-07-23 22:18:42 +0530

    reserach area classification and company details

---
+++ src/com/finvendor/model/ResearchAreaCompanyDetails.java
@@ -0,0 +1,67 @@
+package com.finvendor.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="rsch_area_company_dtls")
+public class ResearchAreaCompanyDetails implements Serializable {
+
+	private static final long serialVersionUID = -3751449132568377457L;
+	
+	@Id
+    @Column(name="company_id")
+	private String compnayId;
+	
+	@Column(name="company_name")
+	private String companyName;
+	
+	@ManyToOne(targetEntity=ResearchSubArea.class, fetch=FetchType.EAGER)
+	@JoinColumn(name="rsch_sub_area_id", nullable=false)
+	private ResearchSubArea researchSubArea;
+	
+	@ManyToOne(targetEntity=ResearchAreaStockClassification.class, fetch=FetchType.EAGER)
+	@JoinColumn(name="stock_class_type_id", nullable=false)
+	private ResearchAreaStockClassification researchAreaStockClassification;
+
+	public String getCompnayId() {
+		return compnayId;
+	}
+
+	public void setCompnayId(String compnayId) {
+		this.compnayId = compnayId;
+	}
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+
+	public ResearchSubArea getResearchSubArea() {
+		return researchSubArea;
+	}
+
+	public void setResearchSubArea(ResearchSubArea researchSubArea) {
+		this.researchSubArea = researchSubArea;
+	}
+
+	public ResearchAreaStockClassification getResearchAreaStockClassification() {
+		return researchAreaStockClassification;
+	}
+
+	public void setResearchAreaStockClassification(ResearchAreaStockClassification researchAreaStockClassification) {
+		this.researchAreaStockClassification = researchAreaStockClassification;
+	}
+
+}
+ 
