e8892697cba8ac38cc75a699ee9b52fa9b2f2178
yogiraj <jbytrain@gmail.com>
2018-06-17 00:10:40 +0530

    Resolve compilation error

--- src/com/finvendor/common/exception/FvBusinessException.java
+++ src/com/finvendor/common/exception/FvBusinessException.java
@@ -1,7 +1,28 @@
-package com.finvendor.common.exception;
-/** * Application checked excpetion businedd issue Use (Abstract class), subclass this * exception for each specific business exception *  * @author Ayush */
-public abstract class FvBusinessException extends FvRootException {	private static final long serialVersionUID = 1L;
-	public FvBusinessException() {		super();	}
-	public FvBusinessException(final String message) {		super(message);	}
-	public FvBusinessException(final Throwable cause) {		super(cause);	}
-	public FvBusinessException(final String message, final Throwable cause) {		super(message, cause);	}}
+package com.finvendor.common.exception;
+
+/**
+ * Application checked excpetion businedd issue Use (Abstract class), subclass
+ * this exception for each specific business exception
+ * 
+ * @author Ayush
+ */
+
+public abstract class FvBusinessException extends FvRootException {
+	private static final long serialVersionUID = 1L;
+
+	public FvBusinessException() {
+		super();
+	}
+
+	public FvBusinessException(final String message) {
+		super(message);
+	}
+
+	public FvBusinessException(final Throwable cause) {
+		super(cause);
+	}
+
+	public FvBusinessException(final String message, final Throwable cause) {
+		super(message, cause);
+	}
+}
