0eaca44bf74782379ad128f6e3eaad1350fbfdd8
yogiraj <jbytrain@gmail.com>
2018-06-16 21:20:35 +0530

    Code improvement-2

---
+++ src/com/finvendor/server/common/infra/scheduler/job/SchdulerJob.java
@@ -0,0 +1,7 @@
+package com.finvendor.server.common.infra.scheduler.job;
+
+import org.quartz.Job;
+
+public abstract class SchdulerJob implements Job {
+
+}
---
+++ src/com/finvendor/server/common/infra/scheduler/service/ISchedulerService.java
@@ -0,0 +1,6 @@
+package com.finvendor.server.common.infra.scheduler.service;
+
+public interface ISchedulerService {
+
+	void scheduleIt() throws Exception;
+}
