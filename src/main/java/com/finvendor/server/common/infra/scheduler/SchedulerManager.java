77bf2a1dbdd8864217bb560ef63a632b0a6d666d
yogiraj <jbytrain@gmail.com>
2018-11-03 14:23:55 +0530

    Commented unused code rather delete

--- src/main/java/com/finvendor/server/common/infra/scheduler/SchedulerManager.java
+++ src/main/java/com/finvendor/server/common/infra/scheduler/SchedulerManager.java
@@ -1,41 +1,41 @@
-package com.finvendor.server.common.infra.scheduler;
-
-import org.quartz.JobDetail;
-import org.quartz.Scheduler;
-import org.quartz.SchedulerException;
-import org.quartz.Trigger;
-import org.quartz.impl.StdSchedulerFactory;
-
-/**
- * 
- * @author ayush on May 12, 2018
- */
-public class SchedulerManager {
-	private static volatile SchedulerManager instance;
-	private static Scheduler scheduler;
-
-	private SchedulerManager() {
-
-	}
-
-	public static SchedulerManager getInstance() throws SchedulerException {
-		if (instance == null) {
-			synchronized (SchedulerManager.class) {
-				if (instance == null) {
-					instance = new SchedulerManager();
-					scheduler = new StdSchedulerFactory().getScheduler();
-					scheduler.start();
-				}
-			}
-		}
-		return instance;
-	}
-
-	public void scheduleIt(JobDetail job, Trigger trigger) throws SchedulerException {
-		scheduler.scheduleJob(job, trigger);
-	}
-
-	public void shutdown() throws SchedulerException {
-		scheduler.shutdown();
-	}
-}
+//package com.finvendor.server.common.infra.scheduler;
+//
+//import org.quartz.JobDetail;
+//import org.quartz.Scheduler;
+//import org.quartz.SchedulerException;
+//import org.quartz.Trigger;
+//import org.quartz.impl.StdSchedulerFactory;
+//
+///**
+// *
+// * @author ayush on May 12, 2018
+// */
+//public class SchedulerManager {
+//	private static volatile SchedulerManager instance;
+//	private static Scheduler scheduler;
+//
+//	private SchedulerManager() {
+//
+//	}
+//
+//	public static SchedulerManager getInstance() throws SchedulerException {
+//		if (instance == null) {
+//			synchronized (SchedulerManager.class) {
+//				if (instance == null) {
+//					instance = new SchedulerManager();
+//					scheduler = new StdSchedulerFactory().getScheduler();
+//					scheduler.start();
+//				}
+//			}
+//		}
+//		return instance;
+//	}
+//
+//	public void scheduleIt(JobDetail job, Trigger trigger) throws SchedulerException {
+//		scheduler.scheduleJob(job, trigger);
+//	}
+//
+//	public void shutdown() throws SchedulerException {
+//		scheduler.shutdown();
+//	}
+//}
--- src/main/java/com/finvendor/server/common/infra/scheduler/service/ISchedulerService.java
+++ src/main/java/com/finvendor/server/common/infra/scheduler/service/ISchedulerService.java
@@ -1,6 +1,6 @@
-package com.finvendor.server.common.infra.scheduler.service;
-
-public interface ISchedulerService {
-
-	void scheduleIt() throws Exception;
-}
+//package com.finvendor.server.common.infra.scheduler.service;
+//
+//public interface ISchedulerService {
+//
+//	void scheduleIt() throws Exception;
+//}
