fc39fe4d87ee692184651b35951066cee2d98ed0
ayush <jbytrain@gmail.com>
2019-04-22 00:24:11 +0530

    feed-1

---
+++ src/main/java/com/finvendor/api/resources/dff/service/externalfeed/earningpreview/impl/AllEarningPreviewFeed.java
@@ -0,0 +1,15 @@
+package com.finvendor.api.resources.dff.service.externalfeed.earningpreview.impl;
+
+import com.finvendor.api.resources.dff.service.externalfeed.earningpreview.EarningPreviewFeed;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class AllEarningPreviewFeed implements EarningPreviewFeed {
+
+    @Override
+    public int feed(String filePath) throws Exception {
+        return 0;
+    }
+}
