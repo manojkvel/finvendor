798e854afdc060216f176ffbddc40d4d1b1f4b1e
ayush <jbytrain@gmail.com>
2019-06-05 13:24:14 +0530

    Delete unused codes

--- src/main/java/com/finvendor/service/MailService.java
+++
@@ -1,8 +0,0 @@
-//package com.finvendor.service;
-//
-//import javax.mail.MessagingException;
-//
-//public interface MailService {
-//
-//	public void sendMail(String from,String to,String subject,String message) throws MessagingException;
-//}
