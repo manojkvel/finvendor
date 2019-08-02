b9792d61729076008261099dba52537c6d3f3c46
ayush <jbytrain@gmail.com>
2019-08-02 22:35:05 +0530

    Test

--- src/main/java/com/finvendor/api/example/controller/ExampleController.java
+++ src/main/java/com/finvendor/api/example/controller/ExampleController.java
@@ -16,7 +16,6 @@ import javax.validation.constraints.Size;
 import java.util.List;
 
 /**
- * 
  * @author ayush on April 30, 2018
  */
 @RestController
@@ -24,38 +23,45 @@ import java.util.List;
 @Validated
 public class ExampleController {
 
-	private ExampleService exampleService;
+    private ExampleService exampleService;
 
-	@Autowired
-	public ExampleController(ExampleService exampleService) {
-		this.exampleService = exampleService;
-	}
+    @Autowired
+    public ExampleController(ExampleService exampleService) {
+        this.exampleService = exampleService;
+    }
 
-	@PostMapping(value = "/examples", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-	public void saveExample(@Valid @RequestBody ExampleRequestDto exampleRequestDto) throws WebApiException {
-		final Example example1Entity = new Example();
-		example1Entity.setId(exampleRequestDto.getId());
-		example1Entity.setName(exampleRequestDto.getName());
-		example1Entity.setPhone(exampleRequestDto.getPhone());
-		exampleService.saveOrUpdateExample1(example1Entity);
-	}
+    @GetMapping(value = "/hello")
+    public ResponseEntity<String> sayHello() {
+        return new ResponseEntity<>("Hello", HttpStatus.OK);
+    }
 
-	@GetMapping(value = "/examples", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<List<ExampleRequestDto>> findAllExample() throws WebApiException {
-		List<ExampleRequestDto> allExample = exampleService.findAllExample();
-		return new ResponseEntity<>(allExample, HttpStatus.OK);
-	}
+    @PostMapping(value = "/examples", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void saveExample(@Valid @RequestBody ExampleRequestDto exampleRequestDto) throws WebApiException {
+        final Example example1Entity = new Example();
+        example1Entity.setId(exampleRequestDto.getId());
+        example1Entity.setName(exampleRequestDto.getName());
+        example1Entity.setPhone(exampleRequestDto.getPhone());
+        exampleService.saveOrUpdateExample1(example1Entity);
+    }
 
-	@GetMapping(value = "/examples/{exampleId}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ExampleRequestDto findExample(@PathVariable @Size(min = 1, max = 3, message = "example id must be [1-3] digit max ") String exampleId) throws WebApiException {
-		ExampleRequestDto pojo=new ExampleRequestDto();
-	    pojo.setId(1);
-	    pojo.setName("Dummy");
+    @GetMapping(value = "/examples", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<ExampleRequestDto>> findAllExample() throws WebApiException {
+        List<ExampleRequestDto> allExample = exampleService.findAllExample();
+        return new ResponseEntity<>(allExample, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/examples/{exampleId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ExampleRequestDto findExample(
+            @PathVariable @Size(min = 1, max = 3, message = "example id must be [1-3] digit max ") String exampleId)
+            throws WebApiException {
+        ExampleRequestDto pojo = new ExampleRequestDto();
+        pojo.setId(1);
+        pojo.setName("Dummy");
         return pojo;
     }
 
-	@RequestMapping(value = "/examples/{exampleId}", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-	public void updateExample(@PathVariable String exampleId, @RequestBody ExampleRequestDto exampleRequestDto) throws WebApiException {
-		exampleService.updateExample(exampleRequestDto);
-	}
+    @RequestMapping(value = "/examples/{exampleId}", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void updateExample(@PathVariable String exampleId, @RequestBody ExampleRequestDto exampleRequestDto) throws WebApiException {
+        exampleService.updateExample(exampleRequestDto);
+    }
 }
