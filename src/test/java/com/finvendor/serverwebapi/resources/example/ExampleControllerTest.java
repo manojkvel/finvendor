ca719681c3d3df8dc768f79432981f810e13faf9
ayush <jbytrain@gmail.com>
2019-09-14 23:53:20 +0530

    FV-000 Code for testing

--- src/test/java/com/finvendor/serverwebapi/resources/example/ExampleControllerTest.java
+++ src/test/java/com/finvendor/serverwebapi/resources/example/ExampleControllerTest.java
@@ -4,6 +4,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.finvendor.api.example.controller.ExampleController;
 import com.finvendor.api.example.dto.ExampleRequestDto;
 import com.finvendor.api.example.service.ExampleService;
+import com.finvendor.common.response.ApiResponse;
+import com.finvendor.common.util.JsonUtil;
+import io.restassured.RestAssured;
+import io.restassured.response.ResponseBody;
+import io.restassured.specification.RequestSpecification;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.util.EntityUtils;
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
@@ -13,6 +21,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -27,48 +36,94 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class ExampleControllerTest {
     private MockMvc mockMvc;
 
-    @Mock
-    ExampleService exampleService;
-
     @InjectMocks
     ExampleController exampleController;
+
+    @Mock
+    ExampleService exampleService;
 
     @Before
     public void init() {
         MockitoAnnotations.initMocks(this);
-        mockMvc = MockMvcBuilders
-                .standaloneSetup(exampleController)
-                .build();
+        mockMvc = MockMvcBuilders.standaloneSetup(exampleController).build();
     }
 
     @Test
-    public void test_get_all_examples() throws Exception {
-        ExampleRequestDto e1=new ExampleRequestDto();
+    public void test_findAllExamples() throws Exception {
+        ExampleRequestDto e1 = new ExampleRequestDto();
         e1.setId(1);
-        e1.setName("Mike");
-        e1.setPhone("123456789");
-        ExampleRequestDto e2=new ExampleRequestDto();
-        e2.setId(2);
-        e2.setName("Tessy");
-        e2.setPhone("123456789");
+        e1.setName("User1");
+        e1.setPhone("11");
 
-        List<ExampleRequestDto> examples = Arrays.asList(e1,e2);
+        ExampleRequestDto e2 = new ExampleRequestDto();
+        e2.setId(2);
+        e2.setName("User2");
+        e2.setPhone("22");
+
+        List<ExampleRequestDto> examples = Arrays.asList(e1, e2);
         when(exampleService.findAllExample()).thenReturn(examples);
-        mockMvc.perform(get("/system/api/findallexample"))
+
+        //Response Json: {"code":"fv-200","message":"Success","data":[{"id":1,"name":"User1","phone":"11"},{"id":2,"name":"User2","phone":"22"}]}
+        mockMvc.perform(get("/api/examples"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
-                .andExpect(jsonPath("$", hasSize(2)))
-                .andExpect(jsonPath("$[0].id", is(1)))
-                .andExpect(jsonPath("$[0].name", is("Ayush")))
-                .andExpect(jsonPath("$[1].id", is(2)))
-                .andExpect(jsonPath("$[1].name", is("Danny")));
+                .andExpect(jsonPath("$.data").isArray())
+                .andExpect(jsonPath("$.data", hasSize(2)))
+                .andExpect(jsonPath("code", is("fv-200")))
+                .andExpect(jsonPath("message", is("Success")))
+                .andExpect(jsonPath("$.data[*].id", Matchers.containsInAnyOrder(1, 2)))
+                .andExpect(jsonPath("$.data[*].name", Matchers.containsInAnyOrder("User1", "User2")))
+                .andExpect(jsonPath("$.data[*].phone", Matchers.containsInAnyOrder("11", "22")));
+
+        RestAssured.baseURI ="http://localhost:9999/api/examples";
+        RequestSpecification request = RestAssured.given();
+        ResponseBody body = request.get("http://localhost:9999/api/examples").getBody();
+        ApiResponse as = body.as(ApiResponse.class);
+        System.out.println(as);
+        //        //Define a postRequest request
+//        HttpPost postRequest = new HttpPost("http://localhost:9999/api/examples");
+//
+//        //Set the API media type in http content-type header
+//        postRequest.addHeader("content-type", "application/json");
+//
+//        //Set the request post body
+//        StringEntity userEntity = new StringEntity(JsonUtil.createJsonFromObject(e1));
+//        postRequest.setEntity(userEntity);
+//
+//        //Send the request; It will immediately return the response in HttpResponse object if any
+//        HttpResponse response1 = HttpClientBuilder.create().build().execute(postRequest);
+//
+//        //verify the valid error code first
+//        int statusCode = response1.getStatusLine().getStatusCode();
+//        if (statusCode != 200) {
+//            throw new RuntimeException("Failed with HTTP error code : " + statusCode);
+//        }
+
+//        // Given
+//        HttpUriRequest request = new HttpGet("http://localhost:9999/api/examples");
+//
+//        // When
+//        CloseableHttpResponse response = HttpClientBuilder.create().build().execute(request);
+//
+//        // Then
+//        ApiResponse<String, List<ExampleRequestDto>> resource = retrieveResourceFromResponse(response);
+//
+//        Assert.assertThat("User1", Matchers.is(resource.getData().get(0).getName()));
+    }
+
+    @SuppressWarnings("unchecked")
+    private static ApiResponse<String, List<ExampleRequestDto>> retrieveResourceFromResponse(
+            CloseableHttpResponse response) throws IOException {
+        String jsonFromResponse = EntityUtils.toString(response.getEntity());
+        return (ApiResponse<String, List<ExampleRequestDto>>) JsonUtil.convertJsonToPojo(jsonFromResponse, ApiResponse.class);
     }
 
     @Test
-    public void test_save_example() throws Exception {
+    public void test_saveExample() throws Exception {
         ExampleRequestDto exampleRequestDto = new ExampleRequestDto();
         exampleRequestDto.setId(1);
-        exampleRequestDto.setName("Danny");
+        exampleRequestDto.setName("User1");
+        exampleRequestDto.setPhone("11");
         doNothing().when(exampleService).saveOrUpdateExample(exampleRequestDto);
 
         mockMvc.perform(
@@ -80,7 +135,7 @@ public class ExampleControllerTest {
     /*
      * converts a Java object into JSON representation
      */
-    public static String asJsonString(final Object obj) {
+    private static String asJsonString(final ExampleRequestDto obj) {
         try {
             return new ObjectMapper().writeValueAsString(obj);
         } catch (Exception e) {
